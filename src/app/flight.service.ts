e6e8467abc0fc10f8bb2b9ece1d9088acc15ae19
melkihaythem <melkihaythem@users.noreply.github.com>
2019-01-07 18:00:02 +0100

    MoreInfo

--- src/app/flight.service.ts
+++ src/app/flight.service.ts
@@ -24,9 +24,30 @@ export class FlightService {
   adulte: string;
   room: string;
   rate: string;
-  tken = 'WYEEvqZoiGPHyZXkjksioIAB9F2d';
+  tken = 'gHW42t4jBVDzOWE4Sh473gNHyIxu';
   result: any;
+
+
+
+
+
+  ville0:string;
+  dat:Date;
+  ville1:string;
+  dat1:Date;
+  name:string;
+  latitude:any;
+  longitude:any;
+
+
+
+
   constructor(private http: HttpClient) { }
+
+
+
+
+
 
   getApi(depart, arrive, dateAller, dateRetour, travelClass, adult, children): Observable<any> {
 
@@ -43,5 +64,49 @@ export class FlightService {
       .set('Content-Type', 'application/json')
       .append('Authorization', 'Bearer ' + this.tken);
     return this.http.get('https://test.api.amadeus.com/v1/shopping/hotel-offers?cityCode=' + villeh + '&checkInDate=' + checkin + '&checkOutDate=' + checkout + '&adults=' + adulte + '&radius=5&radiusUnit=KM&roomQuantity=' + room + '&ratings=' + rate + '&includeClosed=false&bestRateOnly=true&sort=NONE', { headers })
+  }
+
+
+
+
+
+
+
+  flight_MBD(ville0,dat):Observable<any>{
+
+    var headers = new HttpHeaders()
+
+    .set('Content-Type', 'application/json')
+    .append('Authorization', 'Bearer ' + this.tken);
+
+    return this.http.get('https://test.api.amadeus.com/v1/travel/analytics/air-traffic/booked?originCityCode='+ville0+'&period='+dat,{ headers });
+  }
+
+  flight_BTP(ville1,dat1):Observable<any>{
+
+    var headers = new HttpHeaders()
+
+    .set('Content-Type', 'application/json')
+    .append('Authorization', 'Bearer ' + this.tken);
+
+    return this.http.get('https://test.api.amadeus.com/v1/travel/analytics/air-traffic/busiest-period?cityCode='+ville1+'&direction=ARRIVING&period='+dat1,{ headers });
+
+  }
+  getApiH(name):Observable<any>{
+
+    return this.http.get('https://api.opencagedata.com/geocode/v1/json?q='+name+'&key=0018fc6432154dee967c50c6ee2010dc');
+    
+  }
+
+  Airport_NR():Observable<any>{
+
+    var headers = new HttpHeaders()
+
+    .set('Content-Type', 'application/json')
+    .append('Authorization', 'Bearer ' + this.tken);
+
+    return this.http.get('https://test.api.amadeus.com/v1/reference-data/locations/airports?latitude='+this.latitude+'&longitude='+this.longitude,{ headers });
+
+ 
   }
 }
--- src/app/option/option.component.ts
+++ src/app/option/option.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FlightService } from '../flight.service';
 
 @Component({
   selector: 'app-option',
@@ -7,9 +8,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OptionComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+    resFMBD: any;
+    public city: String;
+    public date: Date;
+    dat: Date;
+    ville0: any;
+  
+    showResult = false;
+  
+    resFBTP: any;
+    public city1: String;
+    public date1: Date;
+    dat1: Date;
+    ville1: any;
+  
+  
+  
+    name: string;
+    ville2: string;
+    res;
+    latitude: any;
+    longitude: any;
+  
+  
+    resANR;
+  
+    constructor(private service: FlightService) { }
+  
+  
+    setValue() {
+      console.log(this.showResult)
+      this.showResult = true;
+      console.log(this.showResult)
+      this.ville0 = this.city;
+      console.log(this.ville0);
+      this.dat = this.date;
+      console.log(this.dat);
+  
+      this.service.flight_MBD(this.ville0, this.dat).subscribe(FMBD => {
+        console.log(FMBD);
+        this.resFMBD = FMBD.data;
+  
+  
+      });
+  
+    }
+  
+  
+  
+    setValue1() {
+      this.showResult = true;
+      this.ville1 = this.city1;
+      console.log(this.ville1);
+      this.dat1 = this.date1;
+      console.log(this.dat1);
+      this.service.flight_BTP(this.ville1, this.dat1).subscribe(FBTP => {
+        console.log(FBTP);
+        this.resFBTP = FBTP.data;
+  
+  
+      });
+  
+    }
+  
+    setValue2() {
+      this.name = this.ville2;
+      console.log(this.name);
+  
+      this.service.getApiH(this.name).subscribe(data => {
+        this.res = data.results;
+  
+        this.service.latitude = this.res[0].geometry.lat;
+        this.service.longitude = this.res[0].geometry.lng;
+        console.log(this.res[0].geometry.lng)
+        console.log(this.service.longitude)
+      });
+    }
+  
+    setValue3() {
+      this.showResult = true;
+      this.service.Airport_NR().subscribe(ANR => {
+        this.resANR = ANR.data;
+        console.log(ANR);
+  
+  
+      });
+      //console.log(this.longitude)
+    }
+  
+    ngOnInit() {
+    }
+  
   }
-
-}
+  
